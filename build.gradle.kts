57f19c08c365a79de29be1d5495e454240e48ea3
Steaf23 <steafmobile@gmail.com>
2026-05-25 15:15:24 +0200

    - broken stonecutter setup

---
+++ build.gradle.kts
@@ -0,0 +1,114 @@
+plugins {
+    id("dev.kikugie.loom-back-compat")
+}
+
+version = "${property("mod.version")}+${sc.current.version}"
+base.archivesName = property("mod.id") as String
+
+val requiredJava: JavaVersion = when {
+    sc.current.parsed >= "26.1" -> JavaVersion.VERSION_25
+    sc.current.parsed >= "1.20.5" -> JavaVersion.VERSION_21
+    sc.current.parsed >= "1.18" -> JavaVersion.VERSION_17
+    sc.current.parsed >= "1.17" -> JavaVersion.VERSION_16
+    else -> JavaVersion.VERSION_1_8
+}
+
+fabricApi {
+    configureDataGeneration {
+        client = true
+    }
+}
+
+repositories {
+    // Add repositories to retrieve artifacts from in here.
+    // You should only use this when depending on other mods because
+    // Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
+    // See https://docs.gradle.org/current/userguide/declaring_repositories.html
+    // for more information about repositories.
+}
+
+dependencies {
+    /**
+     * Fetches only the required Fabric API modules to not waste time downloading all of them for each version.
+     * @see <a href="https://github.com/FabricMC/fabric">List of Fabric API modules</a>
+     */
+    fun fapi(vararg modules: String) {
+        for (it in modules) modImplementation(fabricApi.module(it, sc.properties["deps.fabric_api"]))
+    }
+
+    minecraft("com.mojang:minecraft:${sc.current.version}")
+    // Applies Mojang Mappings on obfuscated versions
+    loomx.applyMojangMappings()
+
+    modImplementation("net.fabricmc:fabric-loader:${property("deps.fabric_loader")}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${property("deps.fabric_api")}")
+}
+
+loom {
+    splitEnvironmentSourceSets()
+//
+//    mods {
+//        "ancientwarfare" {
+//            sourceSet sourceSets.main
+//                    sourceSet sourceSets.client
+//        }
+//    }
+
+    fabricModJsonPath = rootProject.file("src/main/resources/fabric.mod.json") // Useful for interface injection
+//    accessWidenerPath = sc.process(
+//        rootProject.file("src/main/resources/template.ct"),
+//        "build/processed.ct"
+//    )
+
+    decompilerOptions.named("vineflower") {
+        options.put("mark-corresponding-synthetics", "1") // Adds names to lambdas - useful for mixins
+    }
+
+    runConfigs.all {
+        vmArgs("-Dmixin.debug.export=true") // Exports transformed classes for debugging
+        runDir = "../../run" // Shares the run directory between versions
+    }
+}
+
+java {
+    withSourcesJar()
+    targetCompatibility = requiredJava
+    sourceCompatibility = requiredJava
+
+    toolchain {
+        vendor = JvmVendorSpec.ADOPTIUM
+        languageVersion = JavaLanguageVersion.of(requiredJava.majorVersion)
+    }
+}
+
+tasks {
+    processResources {
+        fun MutableMap<String, String>.register(key: String, property: String) {
+            val value: String = sc.properties[property]
+            inputs.property(key, value)
+            set(key, value)
+        }
+
+        val props = buildMap {
+            register("id", "mod.id")
+            register("name", "mod.name")
+            register("version", "mod.version")
+            register("minecraft", "mod.mc_compat")
+        }
+
+        filesMatching("fabric.mod.json") { expand(props) }
+
+        val mixinJava = "JAVA_${requiredJava.majorVersion}"
+        filesMatching("*.mixins.json") { expand("java" to mixinJava) }
+    }
+
+    // Builds the version into a shared folder in `build/libs/${mod version}/`
+    register<Copy>("buildAndCollect") {
+        group = "build"
+
+        // loomx.mod(Sources)Jar returns the jar task for the applied loom variant
+        from(loomx.modJar.map { it.archiveFile }, loomx.modSourcesJar.map { it.archiveFile })
+        into(rootProject.layout.buildDirectory.file("libs/${project.property("mod.version")}"))
+        dependsOn("build")
+    }
+}
--- src/main/java/io/github/steaf23/ancientwarfare/core/menu/BlockEntityMenuProvider.java
+++ src/main/java/io/github/steaf23/ancientwarfare/core/menu/BlockEntityMenuProvider.java
@@ -1,7 +1,14 @@
 package io.github.steaf23.ancientwarfare.core.menu;
 
+//? if <= 1.21.11 {
+/*import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory;
+
+public interface BlockEntityMenuProvider extends ExtendedScreenHandlerFactory<BlockEntityScreenData> {
+}
+*///?} else {
 import net.fabricmc.fabric.api.menu.v1.ExtendedMenuProvider;
 
 public interface BlockEntityMenuProvider extends ExtendedMenuProvider<BlockEntityScreenData> {
-
 }
+//?}
+
